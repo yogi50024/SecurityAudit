3c31bf579da8e50db901af958e739ef5c8800779
yogi50024 <yogeshvats50024@gmail.com>
2025-09-08 22:33:55 +0530

    Remove docstrings from droplet and firewall functions
    Removed docstrings from get_droplet_info and get_firewall_rules functions.

--- scanner/do_client.py
+++ scanner/do_client.py
@@ -12,17 +12,9 @@ def get_manager():
     return digitalocean.Manager(token=token)
 
 def get_droplet_info():
-    """
-    Fetch all droplets and basic metadata.
-    Returns: list of digitalocean.Droplet objects
-    """
     manager = get_manager()
     return manager.get_all_droplets()
 
 def get_firewall_rules():
-    """
-    Fetch all firewalls and their rules.
-    Returns: list of digitalocean.Firewall objects
-    """
     manager = get_manager()
     return manager.get_all_firewalls()
